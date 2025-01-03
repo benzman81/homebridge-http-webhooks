89922bd3f20d7b73c54ba80798f30af6424b818d
jsiegenthaler <jbsiegenthaler@gmail.com>
2025-01-03 12:26:58 +0100

    Added handling of invalid event values
    Added clean handling of invalid event values. anything invalid will log a warning in Homebridge and no HomeKit updates will occur

--- src/homekit/accessories/HttpWebHookDoorbellAccessory.js
+++ src/homekit/accessories/HttpWebHookDoorbellAccessory.js
@@ -12,12 +12,12 @@ function HttpWebHookDoorbellAccessory(ServiceParam, CharacteristicParam, platfor
   this.type = "doorbell";
   this.name = doorbellConfig["name"];
 
-
-  // first service must be a StatelessProgrammableSwitch to be supported in the Home app. Only need one button for the doorbell
-  this.service = [];
   var single_press = doorbellConfig["single_press"] === undefined ? true : doorbellConfig["single_press"];
   var double_press = doorbellConfig["double_press"] === undefined ? true : doorbellConfig["double_press"];
   var long_press = doorbellConfig["long_press"] === undefined ? true : doorbellConfig["long_press"];
+
+  // first service must be a StatelessProgrammableSwitch to be supported in the Home app. Only need one button for the doorbell
+  this.service = [];
   var button = new Service.StatelessProgrammableSwitch(this.name,'sps');
   button.getCharacteristic(Characteristic.ProgrammableSwitchEvent).setProps(GetDoorbellStatelessSwitchProps(single_press, double_press, long_press));
   this.service.push(button);
@@ -25,6 +25,7 @@ function HttpWebHookDoorbellAccessory(ServiceParam, CharacteristicParam, platfor
 
   // add the Doorbell as a service
   var doorbell = new Service.Doorbell(this.name,'db');
+  doorbell.getCharacteristic(Characteristic.ProgrammableSwitchEvent).setProps(GetDoorbellStatelessSwitchProps(single_press, double_press, long_press));
   this.service.push(doorbell);
   
 
@@ -43,19 +44,32 @@ HttpWebHookDoorbellAccessory.prototype.changeFromServer = function(urlParams) {
     for (var index = 0; index < this.service.length; index++) {
       var serviceName = this.service[index].getCharacteristic(Characteristic.Name).value;
       var serviceSubtype = this.service[index].subtype;
+      var validValues = this.service[index].getCharacteristic(Characteristic.ProgrammableSwitchEvent).props.validValues;
+
       // for service subtypes sps and db, update the value
       // db is the Doorbell, updateValue causes the HomeKit notification to be sent
       // sps is the StatelessProgrammableSwitch, updateValue causes any programmed switch actions to occur
-      if (['db','sps'].includes(serviceSubtype)) {
-        // we need updates for 2 x services, but we only want to do 1 x logging and so log only for the doorbell update
-        if (serviceSubtype === 'db') {
-          this.log(this.name + ": Pressing '%s' with event '%i'", serviceName, urlParams.event);
-          this.service[index].getCharacteristic(Characteristic.ProgrammableSwitchEvent).updateValue(urlParams.event, undefined, Constants.CONTEXT_FROM_WEBHOOK);
+      if ((['db','sps'].includes(serviceSubtype)) ) {
+        if (validValues.includes(Number(urlParams.event))) {
+          // event value is valid
+          // we need updates for 2 x services, but we only want to do 1 x logging and so log only for the doorbell update
+          if (serviceSubtype === 'db') {
+            this.log(this.name + ": Pressing '%s' with event '%i'", serviceName, urlParams.event);
+            this.service[index].getCharacteristic(Characteristic.ProgrammableSwitchEvent).updateValue(urlParams.event, undefined, Constants.CONTEXT_FROM_WEBHOOK);
+          }
+          // for the sps just do the update, no logging
+          if (serviceSubtype === 'sps') {
+            this.service[index].getCharacteristic(Characteristic.ProgrammableSwitchEvent).updateValue(urlParams.event, undefined, Constants.CONTEXT_FROM_WEBHOOK);
+          }
+        } else {
+          // event value is invalid
+          var errorText  = "event value " + urlParams.event + " is outside of valid values: " + validValues;
+          this.log.warn(this.name + ": WARNING: " + errorText);
+          return {
+            "error" : errorText
+          };
         }
-        // for the sps just do the update, no logging
-        if (serviceSubtype === 'sps') {
-          this.service[index].getCharacteristic(Characteristic.ProgrammableSwitchEvent).updateValue(urlParams.event, undefined, Constants.CONTEXT_FROM_WEBHOOK);
-        }
+
       }
      }
   }
@@ -69,13 +83,15 @@ function GetDoorbellStatelessSwitchProps(single_press, double_press, long_press)
   if (single_press && !double_press && !long_press) {
     props = {
       minValue : Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
-      maxValue : Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS
+      maxValue : Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+      validValues : [ Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS ]
     };
   }
   if (single_press && double_press && !long_press) {
     props = {
       minValue : Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
-      maxValue : Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS
+      maxValue : Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS,
+      validValues : [ Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS, Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS ]
     };
   }
   if (single_press && !double_press && long_press) {
@@ -88,25 +104,29 @@ function GetDoorbellStatelessSwitchProps(single_press, double_press, long_press)
   if (!single_press && double_press && !long_press) {
     props = {
       minValue : Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS,
-      maxValue : Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS
+      maxValue : Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS,
+      validValues : [ Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS ]
     };
   }
   if (!single_press && double_press && long_press) {
     props = {
       minValue : Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS,
-      maxValue : Characteristic.ProgrammableSwitchEvent.LONG_PRESS
+      maxValue : Characteristic.ProgrammableSwitchEvent.LONG_PRESS,
+      validValues : [ Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS, Characteristic.ProgrammableSwitchEvent.LONG_PRESS ]
     };
   }
   if (!single_press && !double_press && long_press) {
     props = {
       minValue : Characteristic.ProgrammableSwitchEvent.LONG_PRESS,
-      maxValue : Characteristic.ProgrammableSwitchEvent.LONG_PRESS
+      maxValue : Characteristic.ProgrammableSwitchEvent.LONG_PRESS,
+      validValues : [ Characteristic.ProgrammableSwitchEvent.LONG_PRESS ]
     };
   }
   if (single_press && double_press && long_press) {
     props = {
       minValue : Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
-      maxValue : Characteristic.ProgrammableSwitchEvent.LONG_PRESS
+      maxValue : Characteristic.ProgrammableSwitchEvent.LONG_PRESS,
+      validValues : [ Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS, Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS, Characteristic.ProgrammableSwitchEvent.LONG_PRESS ]
     };
   }
   return props;
